ac9ae2d784f9c05c46ad6b02d205c79730b8a9ee
Omarr <o.labib1995@gmail.com>
2018-07-27 07:35:15 +0200

    force Arabic local

--- app/src/main/java/labib/com/salatmvp/App.java
+++ app/src/main/java/labib/com/salatmvp/App.java
@@ -1,6 +1,10 @@
 package labib.com.salatmvp;
 
 import android.app.Application;
+import android.content.res.Configuration;
+import android.os.Build;
+
+import java.util.Locale;
 
 import javax.inject.Inject;
 
@@ -15,7 +19,6 @@ public class App extends Application {
     @Inject
     DataManager dataManager;
 
-
     static ApplicationComponent component;
 
     @Override
@@ -27,14 +30,30 @@ public class App extends Application {
                 .build();
         component.inject(this);
 
-
-
-
+        updateOrientation();
     }
 
     public static ApplicationComponent getApplicationComponent() {
         return component;
     }
 
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+            updateOrientation();
+    }
+
+    private void updateOrientation() {
+        Configuration configuration = new Configuration();
+        Locale locale = new Locale("ar");
+        Locale.setDefault(locale);
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            configuration.locale = locale;
+        } else {
+            configuration.setLocale(locale);
+        }
+        getBaseContext().getResources().updateConfiguration(configuration, getBaseContext().getResources().getDisplayMetrics());
+
+    }
 
 }
